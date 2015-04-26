962b0049d17350fae827612157df830783cde15e
Lixxia <artzii.laura@gmail.com>
2015-04-26 16:46:12 -0400

    working on sector_dump

--- project/floppy.c
+++ project/floppy.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <stdio.h>
 #include "floppy.h"
 
 /* Initializes the MS-DOS device indicated by name and returns information about the device as a Disk.
@@ -50,7 +51,6 @@ Disk physical_disk(char* name) {
     printf("\nCylinders: %d",floppy->geometry.cylinders);
 
     floppy->floppyDisk = fd;
-    close(fd);
     return floppy;
 }
 
@@ -62,8 +62,17 @@ Allocate the buffer dynamically to match the size of a sector on the given disk.
 Returns 1 if successful, 0 otherwise.
 */
 int sector_read(Disk theDisk, unsigned int logicalSectorNumber, unsigned char* buffer) {
+    unsigned int bps = theDisk->geometry.bytesPerSector;
+    unsigned int offset = logicalSectorNumber*bps;
+    if(lseek(theDisk->floppyDisk, offset, SEEK_SET) < 0) {
+        perror("Error seeking.");
+        return 0;
+    }
+    if(read(theDisk->floppyDisk, buffer, bps) < 0) {
+        perror("Error reading.");
+        return 0;
+    }
 
-    printf("raejwalr: %s",buffer);
     return 1;
 }
 
@@ -76,5 +85,35 @@ Logical sector starts with byte 1234 (4d2 in hex) and buffer contains 32 bytes o
 0x04e2 021 022 023 024 025 026 027 030 031 032 033 034 035 036 037 040
 */
 void sector_dump(Disk theDisk, int logicalSectorNumber, char type) {
+    int bps = theDisk->geometry.bytesPerSector;
+    unsigned char *buffer = (unsigned char*) malloc(bps);
+    int offset = logicalSectorNumber*bps;
+    char *format;
+    int i,j;
 
+    sector_read(theDisk,logicalSectorNumber,buffer);
+
+    if(type == 'c') {
+        // output as char
+        format = "%c";
+        printf("Char\n");
+    }
+    else if(type == 'x') {
+        // output as hex
+        format = "%01x\t";
+        printf("Hex\n");
+    }
+    else if(type == 'o') {
+        // output as octal
+        format = "%o\t";
+        printf("Oct\n");
+    }
+
+    for(i=0;i<32;i++) {
+        printf("%04x\t",offset+(i*16));
+        for (j = 0; j < 16; j++){
+            printf(format, buffer[(i*16) + j]);
+        }
+        printf("\n");
+    }
 }
--- project/main.c
+++ project/main.c
@@ -1,9 +1,15 @@
 #include "floppy.h"
 
-
+// ./main.c disk sector_num type
 int main(int argc, char** argv) {
-    //initialize disk
-    physical_disk(argv[1]);
+    //initialize errything
+    Disk floppy = physical_disk(argv[1]);
+    int sector_num = argv[2];
+    char type = argv[3][0];
+    sector_dump(floppy,sector_num,type);
+
+    close(floppy->floppyDisk); // close fd
+    free(floppy); // free malloc'd disk
 
     return 0;
 }
